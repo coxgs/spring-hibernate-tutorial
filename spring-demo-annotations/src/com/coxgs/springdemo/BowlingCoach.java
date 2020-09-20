5e6c9dc951e6d8c7e47df684073267f38f0373b5
Geoff Cox <slangingcode@gmail.com>
2020-09-20 02:23:43 -0400

    Section 8 - Inject Properties using Annotations

--- spring-demo-annotations/src/com/coxgs/springdemo/BowlingCoach.java
+++ spring-demo-annotations/src/com/coxgs/springdemo/BowlingCoach.java
@@ -1,9 +1,16 @@
 package com.coxgs.springdemo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
 public class BowlingCoach implements Coach {
+	
+	@Value("${foo.email}")
+	private String email;
+	
+	@Value("${foo.team}")
+	private String team;
 
 	@Override
 	public String getDailyWorkout() {
